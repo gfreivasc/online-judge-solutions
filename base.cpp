71e7cb8eaa4a918512bdd1292fa470278861b818
Gabriel Vasconcelos <gfreivasc@gmail.com>
2016-10-13 20:14:45 -0300

    Base source

---
+++ base.cpp
@@ -0,0 +1,20 @@
+// Source by Gabriel Vasconcelos
+// Pay me 10$ per use or satan will curse your soul.
+
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define min(x, y) ((x) < (y)) ? (x) : (y)
+#define max(x, y) ((x) > (y)) ? (x) : (y)
+
+typedef long long int64;
+typedef unsigned long long uint64;
+typedef unsigned uint;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+
+    // Code here
+    
+    return 0;
+}
